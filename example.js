23692d978146a2d57e0fabd001954fd11dcfcf2a
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-09-16 11:16:52 -0500

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,15 @@
+const consume = (a, b, cb) => cb(a, b)
 
+const add = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => a / b
+
+const greeting = (firstName, lastName) => `Hello, my name is ${firstName} ${lastName}! Nice to meet you!`
+
+console.log(consume(2, 2, add)) // 4
+console.log(consume(4, 2, subtract)) // 2
+console.log(consume(10, 16, multiply)) // 160
+console.log(consume(8, 2, divide)) // 4
+
+console.log(consume("Kenny", "Miesner", greeting)); // Hello, my name is Kenny Miesner! Nice to meet you!
